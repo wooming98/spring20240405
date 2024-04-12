77944ff56ff0d95b6b9dfa2ca0c29c386b9b7d5a
anwoomin <askcw39@naver.com>
2024-04-12 10:12:51 +0900

    공통된 요소 관리

---
+++ src/main/java/com/study/controller/Controller15.java
@@ -0,0 +1,31 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("main15")
+public class Controller15 {
+
+    @RequestMapping("sub1")
+    public void sub1() {
+        // /WEB-INF/view/main15/sub1.jsp 로 포워드
+
+    }
+
+    @RequestMapping("sub2")
+    public void sub2() {
+        // /WEB-INF/view/main15/sub1.jsp 로 포워드
+
+    }
+
+    @RequestMapping("sub3")
+    public void sub3() {
+
+    }
+
+    @RequestMapping("sub4")
+    public void sub4() {
+        
+    }
+}
