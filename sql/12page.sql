360a6ff6f968539cc2e51074885b7bfb9c659bbe
anwoomin <askcw39@naver.com>
2024-04-19 12:47:21 +0900

    데이터 복제

---
+++ sql/12page.sql
@@ -0,0 +1,25 @@
+#12page.sql
+
+USE w3schools;
+
+INSERT INTO Customers
+    (CustomerName, ContactName, Address, City, PostalCode, Country)
+SELECT CustomerName,
+       ContactName,
+       Address,
+       City,
+       PostalCode,
+       Country
+FROM Customers;
+
+SELECT COUNT(*)
+FROM Customers;
+
+# 직원 테이블 데이터 추가
+INSERT INTO Employees
+    (LastName, FirstName, BirthDate, Photo, Notes)
+SELECT LastName, FirstName, BirthDate, Photo, Notes
+FROM Employees;
+
+SELECT COUNT(*)
+FROM Employees;
