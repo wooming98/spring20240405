ae52774341301125c1ea349d7f94fff59b2ec09f
anwoomin <askcw39@naver.com>
2024-05-16 14:46:16 +0900

    JavaBean 객체 응답
    json으로 직렬화할때
    get메소드로 프로퍼티가 결정됨

--- src/main/java/com/study/controller/Controller42.java
+++ src/main/java/com/study/controller/Controller42.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import com.study.domain.MyBean411;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +39,28 @@ public class Controller42 {
         return Map.of("name", "흥민",
                 "age", 35,
                 "married", true,
-                "info", Map.of("team", "토트넘", "location", "런던"),
+                "info", Map.of("team", "토트넘",
+                        "location", "런던"),
                 "item", List.of("축구공", "운동화", "유니폼"));
+    }
+
+    @GetMapping("sub4")
+    @ResponseBody
+    public Map<String, Object> method4() {
+        return Map.of("city", "서울",
+                "height", 188.88,
+                "foods", List.of("pizza", "coffee"),
+                "desc", Map.of("team", "london", "location", "uk"),
+                "hasAccess", false);
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public MyBean411 method5() {
+        MyBean411 obj = new MyBean411();
+        obj.setName("이강인");
+        obj.setAge(55);
+
+        return obj; // json으로 직렬화
     }
 }
