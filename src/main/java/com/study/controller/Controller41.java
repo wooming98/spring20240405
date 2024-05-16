c198976a75650201509597ced91bb9c0575b65de
anwoomin <askcw39@naver.com>
2024-05-16 14:05:05 +0900

    json 데이터 타입이 해당 타입으로 파싱됨
    Map, JavaBean 으로 받을 수 있음

--- src/main/java/com/study/controller/Controller41.java
+++ src/main/java/com/study/controller/Controller41.java
@@ -1,6 +1,7 @@
 package com.study.controller;
 
 
+import com.study.domain.MyBean411;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +28,34 @@ public class Controller41 {
     @PostMapping("sub3")
     public void sub3(@RequestBody Map<String, Object> map) {
         System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub4")
+    public void sub4(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+    }
+
+    @PostMapping("sub5")
+    public void sub5(@RequestBody Map<String, Object> map) {
+        System.out.println("map = " + map);
+        Object name = map.get("name");
+        System.out.println("name.getClass() = " + name.getClass());
+
+        Object age = map.get("age");
+        System.out.println("age.getClass() = " + age.getClass());
+
+        Object item = map.get("item");
+        System.out.println("item.getClass() = " + item.getClass());
+
+        Object married = map.get("married");
+        System.out.println("married.getClass() = " + married.getClass());
+
+        Object team = map.get("team");
+        System.out.println("team.getClass() = " + team.getClass());
+    }
+
+    @PostMapping("sub6")
+    public void sub6(@RequestBody MyBean411 bean411) {
+        System.out.println("bean411 = " + bean411);
     }
 }
