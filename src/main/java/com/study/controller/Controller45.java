d452101fab247b98aa65e5684397b5928069c000
anwoomin <askcw39@naver.com>
2024-05-20 16:18:36 +0900

    여러 파일 받기
    MultipartFile 타입으로 받기
    받은 후 c:/Temp 에 저장
    
    여러 파일 받을 때
    배열이 null 인지 확인
    
    각 파일은 size가 0보다 큰 지 확인

--- src/main/java/com/study/controller/Controller45.java
+++ src/main/java/com/study/controller/Controller45.java
@@ -7,17 +7,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/api/main45")
 public class Controller45 {
     @PostMapping("sub1")
     @ResponseBody
     public String sub1(@RequestParam("name") String name,
-                       @RequestParam("file") MultipartFile file) {
+                       @RequestParam("file") MultipartFile file) throws IOException {
 
         System.out.println("name = " + name);
         System.out.println("file.getOriginalFilename() = " + file.getOriginalFilename());
         System.out.println("file.getSize() = " + file.getSize());
+
+        String path = "C:/Temp/" + file.getOriginalFilename();
+        file.transferTo(new File(path));
+
+        return null;
+    }
+
+    @PostMapping("sub2")
+    @ResponseBody
+    public String sub2(@RequestParam("name") String name,
+                       @RequestParam("file[]") MultipartFile[] file) throws IOException {
+        System.out.println("name = " + name);
+        if (file != null && file.length > 0) {
+            for (MultipartFile f : file) {
+                if (f.getSize() > 0) {
+                    String path = "C:/Temp/" + f.getOriginalFilename();
+                    f.transferTo(new File(path));
+                }
+            }
+        }
+
         return null;
     }
 }
